d7e1ca609c4bc51ed787bef6b6b1dc48524cd253
Saralopezlovon <lopezlovonsara@gmail.com>
2019-12-20 09:38:50 +0100

    Add docs

---
+++ docs/precache-manifest.943f889ffe7539fc7a471e02b2ae9d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82af7955690405197dc494cd5ab9d4f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1894a7210c41d24f7133",
+    "url": "./static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "394eb9e6c2a61f397bb8",
+    "url": "./static/js/2.2dd27514.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.2dd27514.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1894a7210c41d24f7133",
+    "url": "./static/js/main.9bc98662.chunk.js"
+  },
+  {
+    "revision": "a757e8854860e7757b23",
+    "url": "./static/js/runtime-main.9b7fd7af.js"
+  }
+]);
